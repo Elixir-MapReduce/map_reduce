0e97ba6f9154a2e80d16305d0bc3c3b500849bc6
ihaveint <ihaveint@gmail.com>
2021-04-28 23:16:48 +0430

    Refactor apply_map function

--- lib/mapper.ex
+++ lib/mapper.ex
@@ -9,34 +9,25 @@ defmodule Mapper do
 		receive do
 			{:add_raw_element, value} -> loop(map_lambda, [value | raw], reduce_lambda)
 			{:set_raw_array, values} -> loop(map_lambda, values, reduce_lambda)
-			{:calc} -> apply_map(map_lambda, raw, reduce_lambda)
+			{:calc} -> apply_map_reduce(map_lambda, raw, reduce_lambda)
 			{:set_map_reduce, map_lambda, reduce_lambda} -> loop(map_lambda, raw, reduce_lambda)
 		end
 	end
 
-	defp apply_map(map_lambda, raw, reduce_lambda) do
+	defp apply_map_reduce(map_lambda, raw, reduce_lambda) do
 		IO.puts(["raw list is ", Enum.join(raw, ",")])                             
-		apply_map(map_lambda, raw, [], reduce_lambda)
-	end
-
-	defp apply_map(_map_lambda, [], result, reduce_lambda) do
-		# {:ok, file} = File.open(Path.join("test", "output.txt"), [:write])
-		IO.puts(["result before reduce is ",  result |> Enum.reverse |> Enum.join(",")])                             
-
-		final_result = Reducer.reduce(result |> Enum.reverse, reduce_lambda)
-
+		reversed_result = apply_map(map_lambda, raw, [])
+		result = reversed_result |> Enum.reverse
+		IO.puts(["result before reduce: ", Enum.join(result, ",")])
+		final_result = Reducer.reduce(result, reduce_lambda)
 		IO.puts("final result: #{final_result}")
-		# IO.puts(final_result)
-
-		# for value <- result do
-	        # IO.puts value
-	        # IO.write(file, value <> ~s(\n))
-      	# end    
-      	# File.close(file)
-      	# Process.exit(self(), :kill)
 	end
 
-	defp apply_map(map_lambda, [h | t], result, reduce_lambda) do
-		apply_map(map_lambda, t , [map_lambda.(h) | result], reduce_lambda)		
+	defp apply_map(_map_lambda, [], result) do
+		result # reversed
+	end
+
+	defp apply_map(map_lambda, [h | t], result) do
+		apply_map(map_lambda, t , [map_lambda.(h) | result])		
 	end
 end
