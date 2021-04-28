550274d1b345405d310133b1552d9605d4017b19
ihaveint <ihaveint@gmail.com>
2021-04-28 22:57:53 +0430

    Seperate reducer module from mapper module

--- lib/mapper.ex
+++ lib/mapper.ex
@@ -1,4 +1,6 @@
 defmodule Mapper do
+	require Reducer
+
 	def start_link do
 		Task.start_link(fn -> loop([], [], []) end) 
 	end
@@ -17,21 +19,11 @@ defmodule Mapper do
 		apply_map(map_lambda, raw, [], reduce_lambda)
 	end
 
-	defp reducep(short_list = [h | _t], _reduce_lambda) when length(short_list) < 2 do
-		h
-	end
-
-	defp reducep(ff = [h1, h2  | tail], reduce_lambda) do
-		reducep([reduce_lambda.(h1,h2) | tail], reduce_lambda)
-	end
-
-	
-
 	defp apply_map(_map_lambda, [], result, reduce_lambda) do
 		# {:ok, file} = File.open(Path.join("test", "output.txt"), [:write])
 		IO.puts(["result before reduce is ",  result |> Enum.reverse |> Enum.join(",")])                             
 
-		final_result = reducep(result |> Enum.reverse, reduce_lambda)
+		final_result = Reducer.reduce(result |> Enum.reverse, reduce_lambda)
 
 		IO.puts("final result: #{final_result}")
 		# IO.puts(final_result)
---
+++ lib/reducer.ex
@@ -0,0 +1,9 @@
+defmodule Reducer do
+	def reduce(short_list = [h | _t], _reduce_lambda) when length(short_list) < 2 do
+		h
+	end
+
+	def reduce([h1, h2  | tail], reduce_lambda) do
+		reduce([reduce_lambda.(h1,h2) | tail], reduce_lambda)
+	end
+end
