8b913775d830d819652d635fbdb222807e3b0433
ihaveint <ihaveint@gmail.com>
2021-04-30 10:16:50 +0430

    Use builtin map and reduce functions instead of self defined ones

--- lib/mapper.ex
+++ lib/mapper.ex
@@ -4,18 +4,15 @@ defmodule Mapper do
   end
 
   def apply_map(map_lambda, list) do
-    apply_map(map_lambda, list, [])
+    Enum.map(list, map_lambda)
+    # apply_map(map_lambda, list, [])
   end
 
-  defp apply_map(_map_lambda, [], result) do
-    result |> Enum.reverse()
-  end
+  # defp apply_map(_map_lambda, [], result) do
+  #   result |> Enum.reverse()
+  # end
 
-  defp apply_map(map_lambda, [h | t], result) do
-    apply_map(map_lambda, t, [map_lambda.(h) | result])
-  end
-
-  # defp list_to_string(list) do
-  #   Enum.join(list, ", ")
+  # defp apply_map(map_lambda, [h | t], result) do
+  #   apply_map(map_lambda, t, [map_lambda.(h) | result])
   # end
 end
