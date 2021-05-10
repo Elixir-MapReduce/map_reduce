2f3bb3fe36ad07daa119a3bde98eba9253fd1504
ihaveint <ihaveint@gmail.com>
2021-05-10 18:14:07 +0430

    Change elixir vesion

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MapReduce.MixProject do
     [
       app: :map_reduce,
       version: "0.1.0",
-      elixir: "~> 1.10.3",
+      elixir: "~> 1.11.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: MapReduce]
