2836818ca8ae62333679ca0183e537fffb3e3f6d
ihaveint <ihaveint@gmail.com>
2021-05-15 08:35:16 +0430

    Publish on hex (#5)
    * Refactor
    
    * Refactor
    
    * Publish on hex
    
    * Update README

--- lib/map_reduce.ex
+++ lib/map_reduce.ex
@@ -10,13 +10,13 @@ defmodule MapReduce do
     worker_pids = Enum.map(1..process_count, fn _ -> GenServer.start(Worker, []) |> elem(1) end)
 
     collection
-    |> assign_jobs(worker_pids, process_count, {:map, map_lambda})
+    |> assign_jobs(worker_pids, {:map, map_lambda})
     |> List.flatten()
     |> Enum.group_by(fn {key, _value} -> key end)
     |> Enum.map(fn {k, v} ->
       {k, Enum.reduce(v, [], fn _x = {_a, b}, acc -> Enum.concat(to_list(b), acc) end)}
     end)
-    |> assign_jobs(worker_pids, process_count, {:reduce, reduce_lambda})
+    |> assign_jobs(worker_pids, {:reduce, reduce_lambda})
     |> Enum.reduce(%{}, fn {key, values}, acc -> Map.merge(%{key => values}, acc) end)
   end
 
@@ -34,16 +34,19 @@ defmodule MapReduce do
     :crypto.hash(:sha, to_string(key)) |> Base.encode16() |> Integer.parse(16) |> elem(0)
   end
 
-  def assign_jobs(partitions, worker_pids, process_count, {job_type, lambda})
-      when is_atom(job_type) do
+  def assign_jobs(partitions, worker_pids, {job_type, lambda})
+  when is_atom(job_type) do
+    
+    workers_count = length(worker_pids)
+
     partitions
     |> Task.async_stream(
       fn partition ->
-        worker_id = get_hash_for(partition) |> Integer.mod(length(worker_pids))
+        worker_id = get_hash_for(partition) |> Integer.mod(workers_count)
         worker_pid = Enum.at(worker_pids, worker_id)
         GenServer.call(worker_pid, {job_type, partition, lambda})
       end,
-      max_concurrency: process_count,
+      max_concurrency: workers_count,
       ordered: false
     )
     |> Enum.to_list()
