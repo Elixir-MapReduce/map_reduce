399ea09762e8648ee9d6b93f58cc897456c1b831
ihaveint <ihaveint@gmail.com>
2021-05-15 08:30:09 +0430

    Publish on hex

--- mix.exs
+++ mix.exs
@@ -7,24 +7,41 @@ defmodule MapReduce.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11.4",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       escript: [main_module: MapReduce],
-      xref: [exclude: [:crypto]]
+      xref: [exclude: [:crypto]],
+      name: "map_reduce",
+      source_url: "https://github.com/Elixir-MapReduce/map_reduce"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "This package allows you to use the MapReduce paradigm to solve a question, 
+    given that you have the functions map and reduce for that problem"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "map_reduce",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Elixir-MapReduce/map_reduce"}
     ]
   end
 end
