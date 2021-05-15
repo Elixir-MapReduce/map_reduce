83ff6e4fe2462b64ad163876b271bf95f82ca4a1
ihaveint <ihaveint@gmail.com>
2021-05-15 19:01:10 +0430

    Change grouping of functions

--- lib/sample_problem_domains.ex
+++ lib/sample_problem_domains.ex
@@ -3,12 +3,12 @@ defmodule SampleDomains do
 
   def word_mapper({_document, words}), do: Enum.map(words, fn word -> {word, 1} end)
 
-  def map_reduce({:word_count}), do: {&word_mapper/1, &word_reducer/1}
-
-  def map_reduce({:page_rank}), do: {&link_mapper/1, &link_reducer/1}
-
   def link_mapper({source, targets}), do: Enum.map(targets, fn target -> {target, source} end)
 
   def link_reducer({key, values}),
     do: {key, Enum.reduce(values, [], fn x, acc -> Enum.concat([x], acc) end)}
+
+  def map_reduce({:word_count}), do: {&word_mapper/1, &word_reducer/1}
+
+  def map_reduce({:page_rank}), do: {&link_mapper/1, &link_reducer/1}
 end
