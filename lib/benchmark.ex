16dc8ad7f4b8895238797228e778184aa294c979
farzin nasiri <farzin@carriot.ir>
2021-07-10 22:58:59 +0430

    benchamrk

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -3,10 +3,16 @@ defmodule Benchmark do
     Benchee.run(
       %{
         # "sequential" => fn -> MapReduce.run(:file, "yelp_academic_dataset_review.json") end,
-        "input1" => fn -> MapReduce.run(:file, "input1.txt") end,
-        "input2" => fn -> MapReduce.run(:file, "input2.txt") end,
-        "input3" => fn -> MapReduce.run(:file, "input3.txt") end,
-        "input4" => fn -> MapReduce.run(:file, "input4.txt") end
+        "input1" => fn -> MapReduce.no_overhead("input1.txt") end,
+        "input2" => fn -> MapReduce.no_overhead("input2.txt") end,
+        "input3" => fn -> MapReduce.no_overhead("input3.txt") end,
+        "input4" => fn -> MapReduce.no_overhead("input4.txt") end
+        # "input5" => fn -> MapReduce.run(:file,"input5.txt") end,
+        # "input6" => fn -> MapReduce.run(:file,"input6.txt") end,
+        # "input1" => fn -> MapReduce.run(:file, "input1.txt") end,
+        # "input2" => fn -> MapReduce.run(:file, "input2.txt") end,
+        # "input3" => fn -> MapReduce.run(:file, "input3.txt") end,
+        # "input4" => fn -> MapReduce.run(:file, "input4.txt") end
         # "input5" => fn -> MapReduce.run(:file,"input5.txt") end,
         # "input6" => fn -> MapReduce.run(:file,"input6.txt") end,
         # "input7" => fn -> MapReduce.run(:file,"input7.txt") end,
